618a3725e0103bb7872606f8c052146712d976f8
ajithvgiri <mail.ajithvgiri@gmail.com>
2020-06-03 03:03:32 +0530

    splash screen added commit

--- app/src/main/java/com/ajithvgiri/kropperdemo/SplashActivity.kt
+++ app/src/main/java/com/ajithvgiri/kropperdemo/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.ajithvgiri.kropperdemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        startActivity(Intent(this,MainActivity::class.java))
+        finish()
     }
 }
