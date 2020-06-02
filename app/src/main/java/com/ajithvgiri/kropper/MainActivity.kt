18a4369784f57c5677260cfece80864d64d58117
ajithvgiri <mail.ajithvgiri@gmail.com>
2020-06-03 01:39:56 +0530

    feat(storage permission & image picking completed commit):

--- app/src/main/java/com/ajithvgiri/kropper/MainActivity.kt
+++ app/src/main/java/com/ajithvgiri/kropper/MainActivity.kt
@@ -1,11 +1,69 @@
 package com.ajithvgiri.kropper
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val PICK_IMAGE_REQUEST_CODE = 100
+        const val READ_EXTERNAL_STORAGE_REQUEST_CODE = 101
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonChooseImage.setOnClickListener {
+            pickImage()
+        }
+    }
+
+    private fun pickImage() {
+        if (ActivityCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(Intent.createChooser(intent, getString(R.string.title_select_picture)), PICK_IMAGE_REQUEST_CODE)
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(READ_EXTERNAL_STORAGE),
+                    READ_EXTERNAL_STORAGE_REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST_CODE) {
+            if (resultCode != Activity.RESULT_OK) {
+                return
+            }
+            val uri = data?.data
+            if (uri != null) {
+                imageView.setImageURI(uri)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            READ_EXTERNAL_STORAGE_REQUEST_CODE -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // pick image after request permission success
+                    pickImage()
+                }
+            }
+        }
     }
 }
