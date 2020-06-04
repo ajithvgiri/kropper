379411e75f5fa3ecfa09322282768a599384d029
ajithvgiri <mail.ajithvgiri@gmail.com>
2020-06-04 19:29:24 +0530

    kropper library added commit

--- app/src/main/java/com/ajithvgiri/kropperdemo/MainActivity.kt
+++ app/src/main/java/com/ajithvgiri/kropperdemo/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.ajithvgiri.kropperdemo
 
 import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.R.attr
 import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.ajithvgiri.kropper.CropImage
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    companion object{
+    companion object {
         const val PICK_IMAGE_REQUEST_CODE = 100
         const val READ_EXTERNAL_STORAGE_REQUEST_CODE = 101
+        const val IMAGE_CROP_REQUEST_CODE = 102
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,14 +34,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun pickImage() {
-        if (ActivityCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
             val intent = Intent()
             intent.type = "image/*"
             intent.action = Intent.ACTION_GET_CONTENT
-            startActivityForResult(Intent.createChooser(intent, getString(R.string.title_select_picture)), PICK_IMAGE_REQUEST_CODE)
+            startActivityForResult(
+                Intent.createChooser(
+                    intent,
+                    getString(R.string.title_select_picture)
+                ), PICK_IMAGE_REQUEST_CODE
+            )
         } else {
-            ActivityCompat.requestPermissions(this, arrayOf(READ_EXTERNAL_STORAGE),
-                    READ_EXTERNAL_STORAGE_REQUEST_CODE)
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(READ_EXTERNAL_STORAGE),
+                READ_EXTERNAL_STORAGE_REQUEST_CODE
+            )
         }
     }
 
@@ -46,12 +65,27 @@ class MainActivity : AppCompatActivity() {
             }
             val uri = data?.data
             if (uri != null) {
-                imageView.setImageURI(uri)
+                performCrop(uri)
+            }
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            val result = CropImage.getActivityResult(data)
+            if (resultCode == Activity.RESULT_OK) {
+                imageView.setImageURI(result.uri)
+                textView.visibility = View.GONE
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                val error = result.error
+                Toast.makeText(this, "$error", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
             READ_EXTERNAL_STORAGE_REQUEST_CODE -> {
@@ -60,6 +94,18 @@ class MainActivity : AppCompatActivity() {
                     pickImage()
                 }
             }
+        }
+    }
+
+    private fun performCrop(imageUri: Uri) {
+        try {
+            // start cropping activity for pre-acquired image saved on the device
+            CropImage.activity(imageUri).start(this)
+        } // respond to users whose devices do not support the crop action
+        catch (anfe: ActivityNotFoundException) {
+            // display an error message
+            val errorMessage = "Whoops - your device doesn't support the crop action!"
+            Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
         }
     }
 }
